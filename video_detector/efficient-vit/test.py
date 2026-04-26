b44b1deb7cfe2eceb0b6744b7b3684ffff8f11a9
Dhruvi Dhulia <dhruvidhulia535@gmail.com>
2026-04-26 10:19:20 +0530

    Initial clean project upload

---
+++ video_detector/efficient-vit/test.py
@@ -0,0 +1,400 @@
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.metrics import auc
+from sklearn.metrics import accuracy_score
+import os
+import cv2
+import numpy as np
+import torch
+from torch import nn, einsum
+
+from sklearn.metrics import f1_score
+from albumentations import Compose, RandomBrightnessContrast, \
+    HorizontalFlip, FancyPCA, HueSaturationValue, OneOf, ToGray, \
+    ShiftScaleRotate, ImageCompression, PadIfNeeded, GaussNoise, GaussianBlur, Rotate
+
+from transforms.albu import IsotropicResize
+
+from utils import get_method, check_correct, resize, shuffle_dataset, get_n_params
+import torch.nn as nn
+import torch.nn.functional as F
+from functools import partial
+from efficient_vit import EfficientViT
+from utils import transform_frame
+import glob
+from os import cpu_count
+import json
+from multiprocessing.pool import Pool
+from progress.bar import Bar
+import pandas as pd
+from tqdm import tqdm
+from multiprocessing import Manager
+from utils import custom_round, custom_video_round
+
+import yaml
+import argparse
+
+
+MODELS_DIR = "models"
+
+# FIXED PATHS
+BASE_DIR = "../deep_fakes"
+DATA_DIR = os.path.join(BASE_DIR, "dataset")
+TEST_DIR = os.path.join(DATA_DIR, "test_set")
+OUTPUT_DIR = os.path.join(MODELS_DIR, "tests")
+
+TEST_LABELS_PATH = os.path.join(DATA_DIR, "dfdc_test_labels.csv")
+
+
+if not os.path.exists(MODELS_DIR):
+    os.makedirs(MODELS_DIR)
+
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
+
+def create_base_transform(size):
+    return Compose([
+        IsotropicResize(
+            max_side=size,
+            interpolation_down=cv2.INTER_AREA,
+            interpolation_up=cv2.INTER_CUBIC
+        ),
+        PadIfNeeded(
+            min_height=size,
+            min_width=size,
+            border_mode=cv2.BORDER_CONSTANT
+        ),
+    ])
+
+
+def save_roc_curves(correct_labels, preds, model_name, accuracy, loss, f1):
+    plt.figure(1)
+    plt.plot([0, 1], [0, 1], 'k--')
+
+    fpr, tpr, th = metrics.roc_curve(correct_labels, preds)
+    model_auc = auc(fpr, tpr)
+
+    plt.plot(
+        fpr,
+        tpr,
+        label="Model_" + model_name + ' (area = {:.3f})'.format(model_auc)
+    )
+
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.title('ROC curve')
+    plt.legend(loc='best')
+
+    plt.savefig(
+        os.path.join(
+            OUTPUT_DIR,
+            model_name + "_" + opt.dataset +
+            "_acc" + str(accuracy * 100) +
+            "_loss" + str(loss) +
+            "_f1" + str(f1) + ".jpg"
+        )
+    )
+    plt.clf()
+
+
+def read_frames(video_path, videos):
+    method = get_method(video_path, DATA_DIR)
+
+    if "Original" in video_path:
+        label = 0.
+
+    elif method == "DFDC":
+        if "real" in video_path.lower():
+         label = 0.0   # Real
+        else:
+            label = 1.0   # Fake
+        # safer for demo mode
+        
+
+    else:
+        label = 1.
+
+    frames_number = len(os.listdir(video_path))
+    frames_per_video = 30
+    frames_interval = int(frames_number / frames_per_video) if frames_number > 0 else 1
+
+    frames_paths = os.listdir(video_path)
+    frames_paths_dict = {}
+
+    for path in frames_paths:
+        for i in range(0, 3):
+            if "_" + str(i) in path:
+                if i not in frames_paths_dict:
+                    frames_paths_dict[i] = [path]
+                else:
+                    frames_paths_dict[i].append(path)
+
+    if frames_interval > 0:
+        for key in frames_paths_dict.keys():
+            if len(frames_paths_dict[key]) > frames_interval:
+                frames_paths_dict[key] = frames_paths_dict[key][::frames_interval]
+
+            frames_paths_dict[key] = frames_paths_dict[key][:frames_per_video]
+
+    video = {}
+
+    for key in frames_paths_dict.keys():
+        for frame_image in frames_paths_dict[key]:
+            image_size = 224
+            transform = create_base_transform(image_size)
+
+            image = transform(
+                image=cv2.imread(os.path.join(video_path, frame_image))
+            )['image']
+
+            if len(image) > 0:
+                if key in video:
+                    video[key].append(image)
+                else:
+                    video[key] = [image]
+
+    videos.append((video, label, video_path))
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--workers',
+        default=10,
+        type=int,
+        help='Number of data loader workers.'
+    )
+
+    parser.add_argument(
+        '--model_path',
+        default='',
+        type=str,
+        metavar='PATH',
+        help='Path to model checkpoint'
+    )
+
+    parser.add_argument(
+        '--dataset',
+        type=str,
+        default='DFDC',
+        help="Dataset to use"
+    )
+
+    parser.add_argument(
+        '--max_videos',
+        type=int,
+        default=-1,
+        help="Maximum number of videos"
+    )
+
+    parser.add_argument(
+        '--config',
+        type=str,
+        help="Configuration file"
+    )
+
+    parser.add_argument(
+        '--efficient_net',
+        type=int,
+        default=0,
+        help="EfficientNet version"
+    )
+
+    parser.add_argument(
+        '--frames_per_video',
+        type=int,
+        default=30,
+        help="Frames per video"
+    )
+
+    parser.add_argument(
+        '--batch_size',
+        type=int,
+        default=32,
+        help="Batch size"
+    )
+
+    opt = parser.parse_args()
+    print(opt)
+
+    with open(opt.config, 'r') as ymlfile:
+        config = yaml.safe_load(ymlfile)
+
+    if opt.efficient_net == 0:
+        channels = 1280
+    else:
+        channels = 2560
+
+    if os.path.exists(opt.model_path):
+        model = EfficientViT(
+            config=config,
+            channels=channels,
+            selected_efficient_net=opt.efficient_net
+        )
+
+        # FIXED CPU LOAD
+        model.load_state_dict(
+            torch.load(
+                opt.model_path,
+                map_location=torch.device("cpu")
+            )
+        )
+
+        model.eval()
+
+        # FIXED DEVICE HANDLING
+        device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu"
+        )
+
+        model = model.to(device)
+
+    else:
+        print("No model found.")
+        exit()
+
+    model_name = os.path.basename(opt.model_path)
+
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+
+    preds = []
+    mgr = Manager()
+    paths = []
+    videos = mgr.list()
+
+    if opt.dataset != "DFDC":
+        folders = ["Original", opt.dataset]
+    else:
+        folders = [opt.dataset]
+
+    for folder in folders:
+        method_folder = os.path.join(TEST_DIR, folder)
+
+        if not os.path.exists(method_folder):
+            print(f"Folder not found: {method_folder}")
+            continue
+
+        for video_folder in os.listdir(method_folder):
+            paths.append(os.path.join(method_folder, video_folder))
+
+    with Pool(processes=opt.workers) as p:
+        with tqdm(total=len(paths)) as pbar:
+            for _ in p.imap_unordered(
+                partial(read_frames, videos=videos),
+                paths
+            ):
+                pbar.update()
+
+    video_names = np.asarray([row[2] for row in videos])
+    correct_test_labels = np.asarray([row[1] for row in videos])
+    videos = np.asarray([row[0] for row in videos])
+
+    preds = []
+
+    bar = Bar('Predicting', max=len(videos))
+
+    f = open(opt.dataset + "_" + model_name + "_labels.txt", "w+")
+
+    for index, video in enumerate(videos):
+        video_faces_preds = []
+        video_name = video_names[index]
+
+        f.write(video_name)
+
+        for key in video:
+            faces_preds = []
+            video_faces = video[key]
+
+            for i in range(0, len(video_faces), opt.batch_size):
+                faces = video_faces[i:i + opt.batch_size]
+                faces = torch.tensor(np.asarray(faces))
+
+                if faces.shape[0] == 0:
+                    continue
+
+                faces = np.transpose(faces, (0, 3, 1, 2))
+
+                # FIXED CPU/GPU SAFE LINE
+                faces = faces.to(device).float()
+
+                pred = model(faces)
+
+                scaled_pred = []
+                for p in pred:
+                    scaled_pred.append(torch.sigmoid(p))
+
+                faces_preds.extend(scaled_pred)
+
+            if len(faces_preds) == 0:
+                continue
+
+            current_faces_pred = sum(faces_preds) / len(faces_preds)
+            face_pred = current_faces_pred.cpu().detach().numpy()[0]
+
+            f.write(" " + str(face_pred))
+            video_faces_preds.append(face_pred)
+
+        bar.next()
+
+        if len(video_faces_preds) > 1:
+            video_pred = custom_video_round(video_faces_preds)
+        elif len(video_faces_preds) == 1:
+            video_pred = video_faces_preds[0]
+        else:
+            video_pred = 0.5
+
+        preds.append([video_pred])
+
+        f.write(
+            " --> " + str(video_pred) +
+            "(CORRECT: " + str(correct_test_labels[index]) + ")\n"
+        )
+
+    f.close()
+    bar.finish()
+
+    loss_fn = torch.nn.BCEWithLogitsLoss()
+
+    tensor_labels = torch.tensor(
+        [[float(label)] for label in correct_test_labels]
+    )
+
+    tensor_preds = torch.tensor(preds)
+
+    loss = loss_fn(
+        tensor_preds,
+        tensor_labels
+    ).numpy()
+
+    accuracy = accuracy_score(
+        custom_round(np.asarray(preds)),
+        correct_test_labels
+    )
+
+    f1 = f1_score(
+        correct_test_labels,
+        custom_round(np.asarray(preds))
+    )
+
+    print(
+        model_name,
+        "Test Accuracy:",
+        accuracy,
+        "Loss:",
+        loss,
+        "F1:",
+        f1
+    )
+
+    save_roc_curves(
+        correct_test_labels,
+        preds,
+        model_name,
+        accuracy,
+        loss,
+        f1
+    )
